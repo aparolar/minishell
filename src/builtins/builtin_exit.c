2a6d2d40e44e4e726427f8fc8795e347f143b420
aparolar <82216543+aparolar@users.noreply.github.com>
2022-09-03 12:32:48 +0200

    Update Sat Sep  3 12:32:48 CEST 2022

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 12:39:29 by aparolar          #+#    #+#             */
-/*   Updated: 2022/09/01 17:13:01 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/09/03 12:32:38 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	builtin_pre_exit(t_command *cmd)
 	if (ft_lstsize(gs_info()->cmd_lst) == 1)
 	{
 		ft_putstr_fd("exit\n", 1);
-		system("lsof | grep \"minishell \" | grep \" CHR \"");
+		//system("lsof | grep \"minishell \" | grep \" CHR \"");
 		exit_controlled(0);
 	}
 }
