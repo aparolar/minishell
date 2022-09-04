fde19b724982047edf522a0575467fab3a753d36
aparolar <aparolar@student.42madrid.com>
2022-09-04 22:40:46 +0200

    Update domingo,  4 de septiembre de 2022, 22:40:46 CEST

--- src/executor/wait_cmds.c
+++ src/executor/wait_cmds.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 12:59:24 by aparolar          #+#    #+#             */
-/*   Updated: 2022/09/04 19:16:05 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/09/04 20:48:27 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ void	wait_cmds(int n, int last_pid)
 	{
 		exit_status = 0;
 		pid = waitpid(0, &exit_status, 0);
-		printf("WIF %i WEX %i WIFS %i\n", WIFEXITED(exit_status), WEXITSTATUS(exit_status), WIFSIGNALED(exit_status));
-		if (WIFEXITED(exit_status))
+		//printf("WIF %i WEX %i WIFS %i\n", WIFEXITED(exit_status), WEXITSTATUS(exit_status), WIFSIGNALED(exit_status));
+		if (WIFEXITED(exit_status) && !WIFSIGNALED(exit_status))
 			exit_status = WEXITSTATUS(exit_status);
-		if (WIFEXITED(exit_status))
-			
+		else if (!WIFEXITED(exit_status) && WIFSIGNALED(exit_status))
+			exit_status = 130;
 		if (pid == last_pid)
 			gs_info()->exit_status = exit_status;
 		n--;
