67639c090e998bef9600c4adfec42534866fe32f
aparolar <82216543+aparolar@users.noreply.github.com>
2022-09-04 19:16:08 +0200

    Update Sun Sep  4 19:16:08 CEST 2022

--- src/executor/wait_cmds.c
+++ src/executor/wait_cmds.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 12:59:24 by aparolar          #+#    #+#             */
-/*   Updated: 2022/09/04 17:10:52 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/09/04 19:16:05 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ void	wait_cmds(int n, int last_pid)
 	int	pid;
 	int	exit_status;
 
-	while (n)// && WIFEXITED(gs_info()->exit_status))
+	while (n)
 	{
 		exit_status = 0;
 		pid = waitpid(0, &exit_status, 0);
+		printf("WIF %i WEX %i WIFS %i\n", WIFEXITED(exit_status), WEXITSTATUS(exit_status), WIFSIGNALED(exit_status));
 		if (WIFEXITED(exit_status))
 			exit_status = WEXITSTATUS(exit_status);
+		if (WIFEXITED(exit_status))
+			
 		if (pid == last_pid)
 			gs_info()->exit_status = exit_status;
 		n--;
