3cd2b63af94f13d36987e3378b867d2e1c91320c
Idoya Castells Portal <idoyacastellsportal@MacBook-Air-de-Idoya.local>
2022-09-08 20:28:37 +0200

    var expansion of heredoc workingwith and without quotes

--- src/executor/heredoc_expand_vars_utils.c
+++ src/executor/heredoc_expand_vars_utils.c
@@ -6,7 +6,7 @@
 /*   By: icastell <icastell@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 18:12:42 by icastell          #+#    #+#             */
-/*   Updated: 2022/09/08 18:16:00 by icastell         ###   ########.fr       */
+/*   Updated: 2022/09/08 20:24:04 by icastell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,28 @@ int	maybe_expand(t_token *token)
 void	remove_quotes(t_token *token)
 {
 	char	*aux;
+	size_t	i;
+	size_t	j;
 
-	aux = ft_strdup(token->start + 1);
-	aux[ft_strlen(aux) - 1] = 0;
+	aux = ft_calloc(ft_strlen(token->start) - 2, sizeof(char));
+	i = 0;
+	j = 0;
+	while (token->start[i] != '\0')
+	{
+		if ((token->start[i] != '\'') && (token->start[i] != '\"'))
+		{
+			aux[j] = token->start[i];
+			j++;
+		}
+		i++;
+	}
+	aux[i] = '\0';
 	token->start = ft_strdup(aux);
 	free(aux);
 	return ;
+	/*aux = ft_strdup(token->start + 1);
+	aux[ft_strlen(aux) - 1] = 0;
+	token->start = ft_strdup(aux);
+	free(aux);
+	return ;*/
 }
